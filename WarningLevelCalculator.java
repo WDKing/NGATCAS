a4a82c4012664ab1b28a1c83c04df59cd99b1658
William King <king.williamd.wdk@gmail.com>
2015-11-15 12:48:11 -0700

    Update WarningLevelCalculator.java

--- WarningLevelCalculator.java
+++ WarningLevelCalculator.java
@@ -1,106 +1,122 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
 import java.util.ArrayList;
+
 /**
- * This class will calculate the warning level based upon the source aircraft and the array list of neary aircraft
- * 
+ *
  * @author Adam
- * @author William King
  */
 public class WarningLevelCalculator {
     Aircraft[] nearList;
     Aircraft thisPlane;
-    //CollisionCalculator collCalc;
     TextComm textComm;
     ArrayList<Collision> collisions;
-    
+    double dist;
     
     public WarningLevelCalculator(Aircraft thisPlane, Aircraft[] nearList){
         this.nearList = nearList;
         this.thisPlane = thisPlane;
-        //collCalc = new CollisionCalculator(mine, nearList);
         textComm = new TextComm();
         collisions = new ArrayList();
     }
-    
+    public WarningLevelCalculator(){
+        
+    }
     /**
-     * Constructor - Private
-     */
-    private WarningLevelCalculator(){}
-    
-    /**
-     * Calculates the distance of the collision point (if any) and set the appropriate
-     * warning level based upon this calculation.
+     * 
+     * @author Adam
      */
     public void parseList(){
-        /*
+        
+        Collision temp;
+        
         for(int i = 0; i < this.nearList.length; i++){
-            
-            if(checkCollision(thisPlane, this.nearList[i])){
-                //use TextComm object to alert necessary interfaces
+            //make sure there is a list to parse
+            if(nearList == null){ break;}
+            //get a collision point
+            temp = detectCollision(thisPlane, nearList[i]);
+            //make sure you have a collision point
+            if(temp != null){
+                //test the point to ensure its relevant
+                if(testCollisionPoint(temp)){
+                    //set warning levels accordingly
+                    if(dist < 100 && dist > 60){
+                        setWarningLevelYellow(nearList[i]);
+                    }
+                    if(dist < 60 && dist > 30){
+                        setWarningLevelOrange(nearList[i]);
+                    }
+                    if(dist < 30){
+                        setWarningLevelRed(nearList[i]);
+                    }
+                }
+                else{
+                setWarningLevelGreen(nearList[i]);
+                }
             }
+            
             else{
-                //set warning of this.nearList[i] to green
+                setWarningLevelGreen(nearList[i]);
             }
-            
         }
-        */  
-        System.out.println("Method Tested");
     }
-    
     /**
-     * Check to see if the aircraft will collide
-     * Math calculations include finding the vector headings
-     * Add aircraft to the nearby aircraft array
-    public Collision detectCollision(Aircraft one, Aircraft two){
-        return null;
-    
-    }
-    
-    /**
-     * Test to see if the collision is a valid warning collision point.
-     * ie. Distance is within warning area, no valid collision
      * 
-     * param collision Collision object that contains the location (if any) of the 
-     *       point of collision
-     * return boolean value of true if the point of collision is a valid point,
-     *       false if it is not a valid point
+     * @param one
+     * @param two
+     * @return 
+     */
+    public Collision detectCollision(Aircraft one, Aircraft two){
+        
+        return null;
+    }
+    
+    /**
+     * 
+     * @param collision
+     * @return 
      */
     public boolean testCollisionPoint(Collision collision){
-        return false;
+        //all headings should be positive and all points of collision
+        //will be in the positive direction from the current plane position
+        //so error checking to make sure dist is positive would be redundant
+        dist = thisPlane.getLocation() - collision.getLocation();
+        if(dist < 100){
+            return true;
+        }
+        else{ return false;}
     }
-    
     /**
-     * Completes all actions to set the current warning level to 
+     * 
+     * @param plane 
      */
     public void setWarningLevelGreen(Aircraft plane){
-        // Set Gui to Green
-        // Send instructions to PeripherallInterface
-        // Send instructions to TextCommunication
+        
     }
-    
     /**
-     * Completes all actions to set the current warning level to Yellow
+     * 
+     * @param plane 
      */
     public void setWarningLevelYellow(Aircraft plane){
-        // Set Gui to Yellow
-        // Send instructions to PeripherallInterface
-        // Send instructions to TextCommunication
+        //generate instructions??
     }
-    
     /**
-     * Completes all actions to set the current warning level to Orange
+     * 
+     * @param plane 
      */
-     public void setWarningLevelOrange(Aircraft plane){
-        // Set Gui to Orange
-        // Send instructions to PeripherallInterface
-        // Send instructions to TextCommunication
+    public void setWarningLevelOrange(Aircraft plane){
+        //generate instructions??
     }
-    
     /**
-     * Completes all actions to set the current warning level to Red
+     * 
+     * @param plane 
      */
-     public void setWarningLevelRed(Aircraft plane){
-        // Set Gui to Red
-        // Send instructions to PeripherallInterface
-        // Send instructions to TextCommunication
+    public void setWarningLevelRed(Aircraft plane){
+        //generate instructions??
     }
 }
